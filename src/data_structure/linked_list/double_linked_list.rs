7c331e300b66da902b0c58d556681612989a7a23
noobHuKai <hukai2943@aliyun.com>
2022-01-04 10:13:04 +0800

    double linked list

--- src/data_structure/linked_list/double_linked_list.rs
+++ src/data_structure/linked_list/double_linked_list.rs
@@ -20,16 +20,6 @@ impl<T: Debug> ListNode<T> {
             next: None,
         }
     }
-    // fn get_index_node<'a>(&'a mut self, cur: usize, index: usize) -> Rc<RefCell<Self>> {
-    //     if cur >= index {
-    //         return Rc::new(RefCell::new(self));
-    //     }
-    //     if let Some(ref mut node) = self.next {
-    //        let a = node.borrow_mut().get_index_node(cur + 1, index);
-    //        return a
-    //     }
-    //     self
-    // }
     fn display(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?} => ", self.data)?;
 
@@ -39,13 +29,28 @@ impl<T: Debug> ListNode<T> {
         write!(f, "")
     }
 }
+fn get_index_node<T>(
+    node: Rc<RefCell<ListNode<T>>>,
+    cur: usize,
+    index: usize,
+) -> Rc<RefCell<ListNode<T>>> {
+    if cur >= index {
+        return node;
+    }
+    if let Some(node1) = &node.borrow_mut().next {
+        let a = get_index_node(node1.clone(), cur + 1, index);
+        return a.clone();
+    }
+    return node;
+}
+
 #[derive(PartialEq, Eq, Clone, Debug)]
 struct List<T> {
     pub head: Option<Rc<RefCell<ListNode<T>>>>,
     pub length: usize,
 }
 
-impl<T: Debug> List<T> {
+impl<T: Debug + Copy> List<T> {
     #[inline]
     fn new() -> Self {
         List {
@@ -59,22 +64,62 @@ impl<T: Debug> List<T> {
     fn push_back(&mut self, data: T) {
         self.insert(self.length, data);
     }
-
+    // 插入
     fn insert(&mut self, index: usize, data: T) {
         let mut new_node = ListNode::new(data);
         self.length += 1;
-        match self.head.take() {
-            Some(head) => {
-                if index == 0 {
-                    new_node.next = Some(head.clone());
-                    let rc = Rc::new(RefCell::new(new_node));
-                    head.borrow_mut().prev = Some(rc.clone());
-                    self.head = Some(rc);
-                }else{
-
-                }
+        if let Some(ref mut head) = self.head {
+            if index == 0 {
+                let head = self.head.take().unwrap();
+                new_node.next = Some(head.clone());
+                let rc = Rc::new(RefCell::new(new_node));
+                head.borrow_mut().prev = Some(rc.clone());
+                self.head = Some(rc);
+            } else {
+                let prev_node = get_index_node(head.clone(), 0, index - 1);
+                new_node.next = prev_node.borrow_mut().next.take();
+                prev_node.clone().borrow_mut().next = Some(Rc::new(RefCell::new(new_node)));
             }
-            None => self.head = Some(Rc::new(RefCell::new(new_node))),
+        } else {
+            self.head = Some(Rc::new(RefCell::new(new_node)))
+        }
+    }
+
+    // 删除
+    fn delete(&mut self, index: usize) {
+        if let Some(ref mut head) = self.head {
+            self.length -= 1;
+            if index == 0 {
+                self.head = head.clone().borrow_mut().next.take();
+            } else if index >= self.length {
+                let prev_node = get_index_node(head.clone(), 0, self.length - 1);
+                prev_node.borrow_mut().next.take();
+            } else {
+                let prev_node = get_index_node(head.clone(), 0, index - 1);
+                let mut next_node = prev_node.borrow_mut().next.take();
+                prev_node.borrow_mut().next = next_node.as_mut().unwrap().borrow_mut().next.take();
+            }
+        }
+    }
+    // 修改
+    fn change(&mut self, mut index: usize, data: T) {
+        if let Some(ref mut head) = self.head {
+            if index >= self.length {
+                index = self.length;
+            }
+            get_index_node(head.clone(), 0, index).borrow_mut().data = data;
+        }
+    }
+    //  查询
+    fn search(&mut self, index: usize) -> Option<T> {
+        if let Some(ref mut head) = self.head {
+            if index >= self.length {
+                return None;
+            }
+            let data = get_index_node(head.clone(), 0, index).borrow().data;
+            return Some(data);
+        } else {
+            return None;
         }
     }
 }
@@ -92,13 +137,27 @@ where
 #[test]
 fn test_double_linked_list() {
     let mut list = List::new();
-    // list.push_back(20);
+    list.push_back(20);
     list.push_front(100);
     list.push_front(10);
-    // list.push_back(21);
-    // list.push_back(200);
-    // list.insert(0, 50);
-    // list.insert(list.length, 40);
-    // list.insert(list.length + 1, 41);
+    list.push_back(21);
+    list.push_back(200);
+    list.insert(0, 50);
+    list.insert(list.length, 40);
+    list.insert(list.length + 1, 41);
     println!("{}", list);
+
+    list.delete(0);
+    list.delete(list.length);
+    list.delete(2);
+    println!("{}", list);
+
+    list.change(0, 1);
+    list.change(list.length, 1);
+    list.change(2, 1);
+    println!("{}", list);
+
+    println!("{:?}", list.search(0));
+    println!("{:?}", list.search(list.length));
+    println!("{:?}", list.search(1));
 }
