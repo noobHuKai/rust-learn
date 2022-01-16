4ad1c1905135715b98f40ddb8ed877313688c9d9
noobHuKai <hukai2943@aliyun.com>
2022-01-16 23:15:37 +0800

    binary tree

--- src/data_structure/tree/binary_search_tree.rs
+++ src/data_structure/tree/binary_search_tree.rs
@@ -1,132 +1,105 @@
 //! # 二叉查找树或者说二叉搜索树(Binary Search Tree)
-use std::{
-    cmp::PartialOrd,
-    fmt::{Debug, Display},
-};
+use std::{cmp::PartialOrd, fmt::Display};
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode<K, V> {
-    pub key: K,
-    pub val: V,
-    pub left: Option<Box<TreeNode<K, V>>>,
-    pub right: Option<Box<TreeNode<K, V>>>,
+#[derive(Debug)]
+struct TreeNode<T> {
+    pub data: T,
+    pub left: Option<Box<TreeNode<T>>>,
+    pub right: Option<Box<TreeNode<T>>>,
 }
-
-impl<K, V> TreeNode<K, V>
+impl<T> TreeNode<T>
 where
-    K: PartialOrd,
-    V: Copy + Display,
+    T: PartialOrd + Display + Copy,
 {
     #[inline]
-    pub fn new(key: K, val: V) -> Self {
+    fn new(data: T) -> Self {
         TreeNode {
-            key,
-            val,
+            data,
             left: None,
             right: None,
         }
     }
-    pub fn insert(&mut self, key: K, val: V) {
-        if key < self.key {
-            if let Some(node) = &mut self.left {
-                node.insert(key, val);
+    fn insert(&mut self, data: T) {
+        if data < self.data {
+            if let Some(left) = &mut self.left {
+                left.insert(data);
             } else {
-                self.left = Some(Box::new(TreeNode::new(key, val)))
+                self.left = Some(Box::new(TreeNode::new(data)));
             }
-        } else if key > self.key {
-            if let Some(node) = &mut self.right {
-                node.insert(key, val);
+        } else if data > self.data {
+            if let Some(right) = &mut self.right {
+                right.insert(data);
             } else {
-                self.right = Some(Box::new(TreeNode::new(key, val)))
+                self.right = Some(Box::new(TreeNode::new(data)));
             }
-        } else { // as change
+        } else { // 等于不做修改
         }
     }
-    pub fn change(&mut self, key: K, val: V) {
-        if key < self.key {
-            if let Some(node) = &mut self.left {
-                node.change(key, val);
-            } else { // not found
-            }
-        } else if key > self.key {
-            if let Some(node) = &mut self.right {
-                node.change(key, val);
-            } else { //not found
-            }
-        } else {
-            // change
-            self.val = val;
-        }
-    }
-    pub fn search(&mut self, key: K) -> Option<V> {
-        if key < self.key {
-            if let Some(node) = &mut self.left {
-                return node.search(key);
+    fn find(&mut self, find_data: T) -> Option<&mut Self> {
+        if find_data < self.data {
+            if let Some(left) = &mut self.left {
+                return left.find(find_data);
             } else {
                 // not found
                 return None;
             }
-        } else if key > self.key {
-            if let Some(node) = &mut self.right {
-                return node.search(key);
+        } else if find_data > self.data {
+            if let Some(right) = &mut self.right {
+                return right.find(find_data);
             } else {
-                //not found
+                // not found
                 return None;
             }
         } else {
-            return Some(self.val);
+            return Some(self);
         }
     }
-    // 删除节点
-    pub fn delete(&mut self, key: K) {
-        if key < self.key {
-            if let Some(node) = &mut self.left {
-                if node.key == key {
-                    match (&mut node.left, &mut node.right) {
-                        (Some(_), Some(_)) => {}
-                        (None, Some(_)) => {}
-                        (Some(_), None) => {}
-                        (None, None) => {
-                            // 左节点要删除，且是叶子节点
-                            self.left = None;
-                        }
-                    }
-                } else {
-                    // 判断左节点是否要删除
-                    node.delete(key);
-                }
-            } else { // not found
-            }
-        } else if key > self.key {
-            if let Some(node) = &mut self.right {
-                // 判断右节点是否要删除
-                if node.key == key {
-                    match (&mut node.left, &mut node.right) {
-                        (Some(_), Some(_)) => {}
-                        (None, Some(_)) => {}
-                        (Some(_), None) => {}
-                        (None, None) => {
-                            // 有节点要删除，且是叶子节点
-                            self.right = None;
-                        }
-                    }
-                } else {
-                    node.delete(key);
-                }
-            } else { // not found
-            }
+    fn find_min(&mut self) -> Option<&mut Self> {
+        if self.left.is_some() {
+            return self.left.as_mut().unwrap().find_min();
+        } else {
+            return Some(self);
         }
-        // } else {
-        //     match (&mut self.left, &mut self.right) {
-        //         (Some(_), Some(_)) => {}
-        //         (None, Some(_)) => {}
-        //         (Some(_), None) => {}
-        //         (None, None) => {
-        //             std::mem::drop(self);
-        //         }
-        //     }
-        // }
     }
+    fn find_max(&mut self) -> Option<&mut Self> {
+        if self.right.is_some() {
+            return self.right.as_mut().unwrap().find_max();
+        } else {
+            return Some(self);
+        }
+    }
+    // fn delete(&mut self, data: T) {
+    //     if data < self.data {
+    //         if let Some(left) = &mut self.left {
+    //             left.delete(data);
+    //         } else {
+    //             // not found
+    //         }
+    //     } else if data > self.data {
+    //         if let Some(right) = &mut self.right {
+    //             right.delete(data);
+    //         } else {
+    //             // not found
+    //         }
+    //     } else {
+    //         if self.left.is_some() && self.right.is_some() {
+    //             // 左右子节点都有
+    //             let temp = self.right.as_mut().unwrap().find_min();
+    //             self.data = temp.unwrap().data;
+    //             self.right.as_mut().unwrap().delete(self.data);
+    //         } else {
+    //             if self.left.is_some() {
+    //                 if let Some(right) = &mut self.right {
+    //                     self.data = right.data;
+    //                     self.left = right.left;
+    //                     self.right = right.right;
+    //                 }
+    //             } else if self.right.is_some() {
+    //             } else {
+    //             }
+    //         }
+    //     }
+    // }
 
     pub fn get_node_num(&self) -> usize {
         let left = match &self.left {
@@ -154,10 +127,9 @@ where
             return right + 1;
         }
     }
-
     // 前序遍历
     fn pre_order(&self) {
-        println!("{}", self.val);
+        println!("{}", self.data);
         if let Some(left) = &self.left {
             left.pre_order()
         }
@@ -171,7 +143,7 @@ where
         if let Some(left) = &self.left {
             left.infix_order()
         }
-        println!("{}", self.val);
+        println!("{}", self.data);
         if let Some(right) = &self.right {
             right.infix_order()
         }
@@ -185,7 +157,7 @@ where
         if let Some(right) = &self.right {
             right.after_order()
         }
-        println!("{}", self.val);
+        println!("{}", self.data);
     }
     // 层序遍历
     fn level_order(&self) {
@@ -193,7 +165,7 @@ where
         queue.push(self); //根结点入队列
         while !queue.is_empty() {
             let a = queue.remove(0);
-            println!("{}", a.val);
+            println!("{}", a.data);
             if let Some(b) = &a.left {
                 queue.push(b.as_ref());
             }
@@ -202,19 +174,74 @@ where
             }
         }
     }
+
+    fn rightmost_child(&mut self) -> Option<Box<TreeNode<T>>> {
+        match self.right {
+            Some(ref mut right) => {
+                if let Some(t) = right.rightmost_child() {
+                    Some(t)
+                } else {
+                    let mut r = self.right.take();
+                    if let Some(ref mut r) = r {
+                        self.right = std::mem::replace(&mut r.left, None);
+                    }
+                    r
+                }
+            }
+            None => None,
+        }
+    }
+}
+
+fn delete<T: PartialOrd+Display+Copy>(mut this: Box<TreeNode<T>>, target: &T) -> Option<Box<TreeNode<T>>> {
+    if target < &this.data {
+        if let Some(left) = this.left.take() {
+            this.left = delete(left, target);
+        }
+        return Some(this);
+    }
+
+    if target > &this.data {
+        if let Some(right) = this.right.take() {
+            this.left = delete(right, target);
+        }
+        return Some(this);
+    }
+
+    if target != &this.data {
+        println!("not found");
+        return None;
+    }
+    match (this.left.take(), this.right.take()) {
+        (None, None) => None,
+        (Some(left), None) => Some(left),
+        (None, Some(right)) => Some(right),
+
+        (Some(mut left), Some(right)) => {
+            if let Some(mut rightmost) = left.rightmost_child() {
+                rightmost.left = Some(left);
+                rightmost.right = Some(right);
+                Some(rightmost)
+            } else {
+                left.right = Some(right);
+                Some(left)
+            }
+        }
+    }
 }
 
 #[test]
-fn test_binary_tree() {
-    let mut tree = TreeNode::new(0, 23);
-    tree.insert(1, 16);
-    tree.insert(2, 3);
-    tree.insert(3, 22);
-    tree.insert(4, 45);
-    tree.insert(5, 37);
-    tree.insert(6, 99);
-    tree.insert(7, 35);
-    tree.insert(8, 40);
+fn test_bst() {
+    let mut tree = TreeNode::new(100);
+
+    tree.insert(50);
+    tree.insert(200);
+    tree.insert(150);
+    tree.insert(210);
+    tree.insert(170);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(10);
 
     println!(
         "***********************************  prev  order  ***********************************"
@@ -235,20 +262,17 @@ fn test_binary_tree() {
         "***********************************  level order  ***********************************"
     );
     tree.level_order();
-    tree.change(8, 41);
-    tree.change(9, 41);
+
+    println!("min value : {:?}", tree.find_min());
+    println!("max value : {:?}", tree.find_max());
 
     println!(
-        "***********************************  change  level order  ***********************************"
+        "***********************************  delete data  ***********************************"
     );
-    tree.level_order();
-
-    tree.delete(8);
+    // tree.delete(data);
+   let mut a= delete(Box::new(tree), &170);
     println!(
-        "***********************************  delete  level order  ***********************************"
+        "***********************************  level order  ***********************************"
     );
-    tree.level_order();
-
-    println!("node num : {}", &tree.get_node_num());
-    println!("depth : {}", &tree.get_depth());
+    a.as_mut().unwrap().level_order();
 }
