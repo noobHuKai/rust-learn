18bf2952c395af36a2ad09bb486117218f17e66d
noobHuKai <hukai2943@aliyun.com>
2022-01-20 11:17:13 +0800

    Vigenere Cipher

--- src/crypto/classical/mod.rs
+++ src/crypto/classical/mod.rs
@@ -1,24 +1,22 @@
 //! #古典密码
 
 // 凯撒加密
-fn caesar_shifts(buff: String, offset: u8) -> String {
+fn caesar_shifts(message: String, offset: u8) -> String {
     let mut out_buff = String::new();
-    for c in buff.bytes() {
-        let a;
+    for mut c in message.bytes() {
         if c >= b'A' && c <= b'Z' {
-            a = (c - b'A' + offset) % 26 + b'A';
+            c = (c - b'A' + offset) % 26 + b'A';
         } else if c >= b'a' && c <= b'z' {
-            a = (c - b'a' + offset) % 26 + b'a';
-        } else {
-            a = c;
+            c = (c - b'a' + offset) % 26 + b'a';
         }
-        out_buff.push(a as char);
+        out_buff.push(c as char);
     }
     out_buff
 }
 
-fn caesar_shifts_map(buff: String, offset: u8) -> String {
-    buff.chars()
+fn caesar_shifts_map(message: String, offset: u8) -> String {
+    message
+        .chars()
         .map(|c| {
             if c >= 'A' && c <= 'Z' {
                 ((c as u8 - b'A' + offset) % 26 + b'A') as char
@@ -31,14 +29,31 @@ fn caesar_shifts_map(buff: String, offset: u8) -> String {
         .collect::<String>()
 }
 // 栅栏加密
-fn rail_fence_cipher(buff: String, step: usize) -> String {
+fn rail_fence_cipher(message: String, step: usize) -> String {
     let mut out_buff = String::new();
     for i in 0..step {
         let mut j = i;
-        while j < buff.len() {
-            out_buff.push_str(&buff[j..j + 1]);
+        while j < message.len() {
+            out_buff.push_str(&message[j..j + 1]);
             j += step
         }
+    }
+    out_buff
+}
+
+// 维吉尼亚密码
+fn vigenere_cipher(message: String, secret: String) -> String {
+    let a = secret.to_uppercase();
+    let secret = a.as_bytes();
+    let mut out_buff = String::new();
+    for (index, mut c) in message.bytes().enumerate() {
+        let i = index % secret.len();
+        if c >= b'A' && c <= b'Z' {
+            c = (c + secret[i] - 2 * b'A') % 26 + b'A';
+        } else if c >= b'a' && c <= b'z' {
+            c = (c + secret[i] - b'a' - b'A') % 26 + b'a';
+        }
+        out_buff.push(c as char);
     }
     out_buff
 }
@@ -56,5 +71,9 @@ fn test_classical_encryption() {
     println!(
         "Rail Fence Cipher result : {:?}",
         rail_fence_cipher("AZaz66".to_string(), 2)
+    );
+    println!(
+        "Vigenere Cipher result : {:?}",
+        vigenere_cipher("HelloWorldHuKai".to_string(), String::from("HuKai"))
     );
 }
